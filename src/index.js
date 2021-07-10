b641a17610e4e468338d2a7ccf3b0803ce08c4c2
Miguel Ricaño <mricanho@gmail.com>
2021-07-10 18:24:11 -0500

    Add categories initializer and index in actions directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
