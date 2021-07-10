fb5897d2d191ee4ef95a8d7c18227e77bc795c57
Miguel Ricaño <mricanho@gmail.com>
2021-07-09 21:29:00 -0500

    Install bulma

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import './App.css';
+import 'bulma/css/bulma.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="main">
+    <div className="buttons">
+      <button className="button is-primary" type="button">Primary</button>
+      <button className="button is-link" type="button">Link</button>
+      <button className="button is-info" type="button">Info</button>
+      <button className="button is-success" type="button">Success</button>
+      <button className="button is-warning" type="button">Warning</button>
+      <button className="button is-danger" type="button">Danger</button>
+      <button className="button is-black" type="button">Black</button>
+      <button className="button is-white" type="button">White</button>
+      <button className="button is-dark" type="button">Dark</button>
+      <button className="button is-light" type="button">Light</button>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
