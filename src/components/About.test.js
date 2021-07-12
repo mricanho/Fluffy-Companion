ea18c8c7513a78254bf0c8cc765160f288c69fd2
Miguel Ricaño <mricanho@gmail.com>
2021-07-12 12:54:50 -0500

    Add about test

---
+++ src/components/About.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from './About';
+
+describe('About', () => {
+  test('renders About component', () => {
+    render(<About />);
+    screen.getByText(/animals/);
+  });
+});
