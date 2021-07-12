eb2b0669a25a2cfb6e78dd45c5c309bd682e95da
Miguel Ricaño <mricanho@gmail.com>
2021-07-12 13:01:08 -0500

    Add ohoto snapshot test

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -5,13 +5,11 @@ import Footer from './Footer';
 
 let container = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
   container = document.createElement('div');
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
   unmountComponentAtNode(container);
   container.remove();
   container = null;
